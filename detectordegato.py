b3f9db9b70ab167b4ba53886dca858900cb6e4d2
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2023-03-11 12:46:06 -0300

    Add files via upload

---
+++ detectordegato.py
@@ -0,0 +1,79 @@
+#CRIANDO SISTEMA DETECTOR DE FRAUDES EM INSTALAÇÕES DE ENERGIA ELÉTRICA
+#USANDO LÓGICA FUZZY
+
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt 
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Variáves de Entrada
+
+consumo = ctrl.Antecedent(np.arange(0, 21, 1), 'Consumo')
+tarifa = ctrl.Antecedent(np.arange(0, 200, 1), 'Tarifa')
+gasto = ctrl.Antecedent(np.arange(0, 60, 1), 'Gasto de Energia')
+resultado = ctrl.Consequent(np.arange(0, 39, 1), 'Resultado Final')
+
+# Criação automática das variáveis fuzzy de Entrada
+consumo.automf(3)
+tarifa.automf(3)
+gasto.automf(3)
+
+consumo.view()
+tarifa.view()
+gasto.view()
+
+# Criação da variável Fuzzy de Saida,
+# 
+resultado['gato'] = fuzz.trimf(resultado.universe, [0, 0, 12])
+resultado['normal'] = fuzz.trimf(resultado.universe, [10, 15, 25])
+resultado['suspeito'] = fuzz.trimf(resultado.universe, [22, 29, 35])
+
+#Criando as regras
+regra1 = ctrl.Rule(consumo['poor'] | tarifa['poor']|gasto['average'], resultado['normal'])
+regra2 = ctrl.Rule(consumo['average'] | tarifa['average']|gasto['good'], resultado['suspeito'])
+regra3 = ctrl.Rule(consumo['poor'] | tarifa['average']|gasto['good'], resultado['gato'])
+
+"""
+regra1.view()
+
+regra2.view()
+
+regra3.view()
+
+"""
+
+resultado_ctrl = ctrl.ControlSystem([regra1, regra2, regra3])
+valorresultado = ctrl.ControlSystemSimulation(resultado_ctrl)
+
+input_consumo = float(input("Digite o valor de consumo: "))
+input_tarifa = int(input("Digite o valor da tarifa de consumo: "))
+input_gasto = float(input("Digite o valor unitário no período: "))
+
+# Dados de entrada
+valorresultado.input['Consumo'] = input_consumo
+valorresultado.input['Tarifa'] = input_tarifa
+valorresultado.input['Gasto de Energia'] = input_gasto
+
+
+#Calculando o valor da conta de energia para o mês
+valor_conta = (input_tarifa*input_gasto)
+print("\n")
+print("O valor total da sua fatura é R$ "+ str(round(valor_conta,2)))
+
+# Calculando o resultado
+valorresultado.compute()
+print("\n")
+print ("Nível de medição: " + str(round(valorresultado.output['Resultado Final'],2)))
+
+if valorresultado.output['Resultado Final'] <=13:
+    print("\n")
+    print("É gato!")
+elif valorresultado.output['Resultado Final'] <=19:
+    print("\n")
+    print("Ligação normal")
+else:
+    print("\n")
+    print("Ligação suspeita")
+
+resultado.view(sim = valorresultado)
